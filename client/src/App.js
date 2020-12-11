d460031e4a1f13eaeae0962f3a3997e6612e82ae
Yuda <yuda10aditya@gmail.com>
2020-12-11 22:01:06 +0700

    modified client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import ChatBox from './components/ChatBox';
+
+function App() {
+  return (
+    <div className="container">
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
